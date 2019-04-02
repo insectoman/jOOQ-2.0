8658b152f1b90b03eb93dd6a69f2bea877af24d4
Piotr Hendzel <phendzel.dev@gmail.com>
2019-04-02 22:51:03 +0200

    Initial commit

---
+++ src/main/java/com/phendzel/configuration/JooqSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.phendzel.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqSampleAppApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1__Init_db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `author`
+(
+    `id`         int NOT NULL,
+    `first_name` varchar(255) DEFAULT NULL,
+    `last_name`  varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
